b9938411eac047ab1443f58a751defd10fdab961
krishnadheeraj <31441766+krishnadheeraj@users.noreply.github.com>
2019-07-30 11:38:47 +0530

    Create BS1Q5.py

---
+++ BS1Q5.py
@@ -0,0 +1,15 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if(a==b and a>c):
+  print(a)
+elif(b==c and b>a):
+  print(b)
+elif(a==c and a>b):
+  print(c)
+elif(a>b and a>c):
+  print(a)
+elif(b>c and b>a):
+  print(b)
+elif(c>a and c>b10):
+  print(c)
